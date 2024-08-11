ff589cc41df057ffe85d4a27437c0d2e86dbea15
unknown <markprince29.mp@gmail.com>
2024-08-11 19:50:00 +0100

    css styling

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Raleway } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={raleway.className}>{children}</body>
+      <body className={raleway.className}>
+        <Header />
+        {children}
+        </body>
     </html>
   );
 }
